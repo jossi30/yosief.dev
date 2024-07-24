ffbc9728ef166441a9a6c94ae5b05951d64dd9f4
jossi30 <yosiefghaylay@gmail.com>
2024-07-24 23:36:54 +0300

    added a work section

---
+++ src/mock/profile.js
@@ -0,0 +1,23 @@
+export const SOCIAL_LINKS = [
+  {
+    link: 'https://x.com/jossi30_',       
+    icon: 'akar-icons:twitter-fill',        
+  },
+  {
+    link: 'https://github.com/jossi30',
+    icon: 'akar-icons:github-fill',
+  },
+  {
+    link: 'https://www.linkedin.com/in/yosief-g-hailay-290277213/',
+    icon: 'akar-icons:linkedin-fill',
+  },
+];
+
+export const HERO_TITLES = ['Yosief G. Hailay', ' A Full Stack Developer', ' A Software Engineer'];
+
+export const aboutParagraph =
+  'Highly motivated and enthusiastic Full Stack Engineer with experience in designing, developing and maintaining web applications using technologies such as Generative AI, Typescript, React, Next.js, Node.js, Python.';
+
+export const contactEmail = 'yosief.hailay@gmail.com';
+
+export const documentTitle = 'Hello I am Yosief G. Hailay | Full Stack Developer | Software Engineer'
---
+++ src/mock/projects.js
@@ -0,0 +1,97 @@
+import { BACKEND_SKILLS, FRONTEND_SKILLS } from './tech-skills';
+
+export const PROJECT_CATEGORY = {
+  ALL: 'all',
+  FULL_STACK: 'full-stack',
+  OPEN_SOURCE: 'open-source',
+};
+
+export const TABS = [
+  {
+    label: 'All',
+    value: PROJECT_CATEGORY.ALL,
+    icon: 'fluent:tab-desktop-20-regular',
+  },
+  {
+    label: 'Full Stack',
+    value: PROJECT_CATEGORY.FULL_STACK,
+    icon: 'bx:code-block',
+  },
+  {
+    label: 'Open Source',
+    value: PROJECT_CATEGORY.OPEN_SOURCE,
+    icon: 'fluent-mdl2:open-source',
+  },
+];
+
+const getSkillByLabel = (skills, techLabel) => skills.find(({ label }) => label === techLabel);
+
+export const PROJECTS = [
+  {
+    imgSrc: './src/assets/images/project.png', // Updated path
+    title: 'Flipkart Clone - E-Commerce Project',
+    techIcons: [
+      getSkillByLabel(FRONTEND_SKILLS, 'HTML'),
+      getSkillByLabel(FRONTEND_SKILLS, 'CSS'),
+      getSkillByLabel(FRONTEND_SKILLS, 'React.js'),
+      getSkillByLabel(FRONTEND_SKILLS, 'Material UI (mui)'),
+      getSkillByLabel(FRONTEND_SKILLS, 'Redux'),
+      getSkillByLabel(BACKEND_SKILLS, 'Node.js'),
+      getSkillByLabel(BACKEND_SKILLS, 'Mongodb'),
+    ],
+    description:
+      'Flipkart Clone is done with top-notch features for the entrepreneur startups like Flipkart. It is build with React Js, Node Js.',
+    repoLink: 'https://github.com/',
+    sourceLink: 'https:///',
+    category: [PROJECT_CATEGORY.FULL_STACK, PROJECT_CATEGORY.OPEN_SOURCE],
+  },
+  {
+    imgSrc: './src/assets/images/developer-portfolio.png', // Updated path
+    title: 'Developer Portfolio',
+    techIcons: [
+      getSkillByLabel(FRONTEND_SKILLS, 'HTML'),
+      getSkillByLabel(FRONTEND_SKILLS, 'CSS'),
+      getSkillByLabel(FRONTEND_SKILLS, 'Tailwind CSS'),
+      getSkillByLabel(FRONTEND_SKILLS, 'Next.js'),
+    ],
+    description:
+      'Simple and classic modern portfolio for who wants to present himself as a software developer. Portfolio build with highly customizable using Tailwind CSS, Next Js.',
+    repoLink: 'https://github.com/',
+    sourceLink: 'https:///',
+    category: [PROJECT_CATEGORY.OPEN_SOURCE],
+  },
+  {
+    imgSrc: './src/assets/images/kitchen-store.png', // Updated path
+    title: 'Kitchen Store - E-Commerce',
+    techIcons: [
+      getSkillByLabel(FRONTEND_SKILLS, 'HTML'),
+      getSkillByLabel(FRONTEND_SKILLS, 'CSS'),
+      getSkillByLabel(FRONTEND_SKILLS, 'Javascript'),
+      getSkillByLabel(FRONTEND_SKILLS, 'Bootstrap'),
+      {
+        label: 'Java',
+        icon: 'logos:java',
+      },
+      getSkillByLabel(BACKEND_SKILLS, 'MySQL'),
+    ],
+    description:
+      'The e-commerce to buy kitchen related items. College final year project base on Servlets and JSP (Java)',
+    repoLink: 'https://github.com/',
+    sourceLink: '',
+    category: [PROJECT_CATEGORY.FULL_STACK, PROJECT_CATEGORY.OPEN_SOURCE],
+  },
+  {
+    imgSrc: './src/assets/images/netflix-clone.png', // Updated path
+    title: 'Netflix Clone - TMDB API Project',
+    techIcons: [
+      getSkillByLabel(FRONTEND_SKILLS, 'HTML'),
+      getSkillByLabel(FRONTEND_SKILLS, 'CSS'),
+      getSkillByLabel(FRONTEND_SKILLS, 'React.js'),
+      getSkillByLabel(BACKEND_SKILLS, 'REST Api'),
+    ],
+    description: 'Netflix clone using react Js. Proving movies details using TMDB API.',
+    repoLink: 'https://github.com/',
+    sourceLink: 'https://@.web.app/',
+    category: [PROJECT_CATEGORY.OPEN_SOURCE],
+  },
+];
